1326879f565f612f520501143e5ab5247bbae3fb
kdelancey <pandaplox@gmail.com>
2017-04-04 23:28:06 -0400

    Added contributor space. Pressing plus brings up a modal, and 'adds' a contributor. Added a finish slide to the create event carousel as well.

--- createjs.js
+++ createjs.js
@@ -10,4 +10,21 @@ $(document).ready(function(){
     $('#previousButton').click( function() {
        $('#creationCarousel').carousel(0);
     });
+    
+    $('#finishButton').click( function() {
+       $('#creationCarousel').carousel(2);
+    });
+    
+    $('#backButton').click( function() {
+       $('#creationCarousel').carousel(1);
+    });
+    
+    $('#contributorAdd').click( function() {
+       $('#contributorModal').modal('show');
+    });
+    
+    $('#modalAddButton').click( function() {
+       $('#contributorBody').append("<div id=\"contributorIcon\" class=\"col pull-left\"><a href=\"#user\"><span class=\"icons\"><span class=\"glyphicon glyphicon-user\"></span></a></div>");
+    });
+    
 });
