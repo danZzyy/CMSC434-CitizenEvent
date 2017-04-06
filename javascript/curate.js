38a7aeaafa5b68d6cd2f01b5ee4d654121d5a43c
kdelancey <pandaplox@gmail.com>
2017-04-06 00:48:23 -0400

    First curate push.

---
+++ javascript/curate.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $(".custom-control-input").click(function(){
+    console.log("here")
+    if($(this).closest("div.data-container").hasClass("selected-photo")){
+      $(this).closest("div.data-container").removeClass("selected-photo")
+      $(this).next().removeClass("selected-check");
+      $(this).next().empty();
+    } else {
+      $(this).closest("div.data-container").addClass("selected-photo")
+      $(this).next().addClass("selected-check")
+      $(this).next().append("<span class='checkmark glyphicon glyphicon-check'/>")
+    }
+  })
+
+  $(".custom-control-input").hover(function(){
+    console.log("here")
+
+      if($(this).closest("div.data-container").hasClass("hover-photo") && !$(this).closest("div.data-container").hasClass("selected-photo")){
+        $(this).closest("div.data-container").removeClass("hover-photo")
+        $(this).next().removeClass("hover-check");
+      } else {
+        $(this).closest("div.data-container").addClass("hover-photo")
+        $(this).next().addClass("hover-check")
+      }
+
+  })
+});
