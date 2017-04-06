6544227f755fcd9b7faddaaef0063239a4e954ee
IsabellaS09 <ems111.es@gmail.com>
2017-04-06 01:41:19 -0400

    No more using iframe

--- javascript/curate.js
+++ javascript/curate.js
@@ -1,27 +1,16 @@
 $(document).ready(function() {
-  $(".custom-control-input").click(function(){
-    console.log("here")
-    if($(this).closest("div.data-container").hasClass("selected-photo")){
-      $(this).closest("div.data-container").removeClass("selected-photo")
-      $(this).next().removeClass("selected-check");
-      $(this).next().empty();
-    } else {
-      $(this).closest("div.data-container").addClass("selected-photo")
-      $(this).next().addClass("selected-check")
-      $(this).next().append("<span class='checkmark glyphicon glyphicon-check'/>")
-    }
+  $("#template-two-button").click(function(){
+
+    $("#template-one").addClass("noDisplay");
+    $("#template-two").removeClass("noDisplay");
   })
 
-  $(".custom-control-input").hover(function(){
-    console.log("here")
+  $("#template-one-button").click(function(){
+    $("#template-two").addClass("noDisplay");
+    $("#template-one").removeClass("noDisplay");
+  })
 
-      if($(this).closest("div.data-container").hasClass("hover-photo") && !$(this).closest("div.data-container").hasClass("selected-photo")){
-        $(this).closest("div.data-container").removeClass("hover-photo")
-        $(this).next().removeClass("hover-check");
-      } else {
-        $(this).closest("div.data-container").addClass("hover-photo")
-        $(this).next().addClass("hover-check")
-      }
-
+  $("#export").click(function(){
+    alert("Sorry this feature is still under construction!");
   })
 });
