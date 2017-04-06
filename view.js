0ecdc4fafbe451cfa5e975aa010fc825daff2fa9
kdelancey <pandaplox@gmail.com>
2017-04-06 01:50:17 -0400

    Hot mess rn. Ella, you mentioned adding edit slides to your carousel.

--- view.js
+++ view.js
@@ -27,4 +27,30 @@ $(document).ready(function(){
        $('#contributorBody').append("<div id=\"contributorIcon\" class=\"col pull-left\"><a href=\"#user\"><span class=\"icons\"><span class=\"glyphicon glyphicon-user\"></span></a></div>");
     });
     
+    $(".custom-control-input").click(function(){
+    console.log("here")
+    if($(this).closest("div.data-container").hasClass("selected-photo")){
+      $(this).closest("div.data-container").removeClass("selected-photo")
+      $(this).next().removeClass("selected-check");
+      $(this).next().empty();
+    } else {
+      $(this).closest("div.data-container").addClass("selected-photo")
+      $(this).next().addClass("selected-check")
+      $(this).next().append("<span class='checkmark glyphicon glyphicon-check'/>")
+    }
+  })
+
+  $(".custom-control-input").hover(function(){
+    console.log("here")
+
+      if($(this).closest("div.data-container").hasClass("hover-photo") && !$(this).closest("div.data-container").hasClass("selected-photo")){
+        $(this).closest("div.data-container").removeClass("hover-photo")
+        $(this).next().removeClass("hover-check");
+      } else {
+        $(this).closest("div.data-container").addClass("hover-photo")
+        $(this).next().addClass("hover-check")
+      }
+
+  })
+    
 });
