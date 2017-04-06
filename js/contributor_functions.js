72c390689e873397e117ea9334f576f9e1483082
IsabellaS09 <ems111.es@gmail.com>
2017-04-06 08:10:37 -0400

    Stlye fixes

--- js/contributor_functions.js
+++ js/contributor_functions.js
@@ -3,5 +3,29 @@ function in_progress(screen) {
 }
 
 function event_information() {
-  
+
 }
+
+$("#twitter").click(function(){
+  $("#twitter-text").text("Connected to Twitter");
+  $("#twitter").prop("disabled", true);
+  $("#pushToproject").prop("disabled", false);
+});
+
+$("#tumblr").click(function(){
+  $("#tumblr-text").text("Connected to Tumblr");
+  $("#tumblr").prop("disabled", true);
+  $("#pushToproject").prop("disabled", false);
+});
+
+$("#facebook").click(function(){
+  $("#fb-text").text("Connected to Facebook");
+  $("#facebook").prop("disabled", true);
+  $("#pushToproject").prop("disabled", false);
+});
+
+$("#insta").click(function(){
+  $("#insta-text").text("Connected to Instagram");
+  $("#insta").prop("disabled", true);
+  $("#pushToproject").prop("disabled", false);
+});
